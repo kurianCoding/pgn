1ddfefdc2bcaabf564c43d3651825fe3f2424aee
kurian <kurian@dev.local>
2021-02-13 18:49:59 +0530

    convert play to string

--- parsePgn.go
+++ parsePgn.go
@@ -1,6 +1,7 @@
 package pgn
 
 import (
+	"fmt"
 	"log"
 	"regexp"
 	"strings"
@@ -28,12 +29,13 @@ import (
 
 1. d4 e6 2. Bf4 Nc6 3. e3 d6 4. Bd3 Qf6 5. Nf3 Bd7 6. Nbd2 O-O-O 7. Qe2 d5 8. O-O-O h6 9. h3 g5 10. Be5 Nxe5 11. dxe5 Qe7 12. Nd4 f6 13. exf6 Nxf6 14. Bb5 Bxb5 15. Qxb5 Ne4 16. Qa5 Nxd2 17. Rxd2 Bg7 18. Nb5 b6 19. Qxa7 Qc5 20. Nd4 Bxd4 21. Rxd4 Rd6 22. Qa8+ Kd7 23. Qxh8 Rc6 24. c3 b5 25. Qh7+ Kd6 26. Qxh6 b4 27. Qxg5 bxc3 28. bxc3 Qxc3+ 29. Kb1 Rb6+ 30. Rb4 Rxb4# 0-1
 */
+type Move struct {
+	White string `json:"white"`
+	Black string `json:"black"`
+}
 type PGN struct {
 	Meta  map[string]string `json:"metadata"`
-	Moves []struct {
-		White string `json:"white"`
-		Black string `json:"black"`
-	} `json:"moves"`
+	Moves []Move            `json:"moves"`
 }
 
 func Parse(pgnbytes []byte) *PGN {
@@ -47,14 +49,27 @@ func Parse(pgnbytes []byte) *PGN {
 		getmeta[key] = value
 	}
 	movslic := regexp.MustCompile(`[0-9]+\.`).Split(pgnstring[len(pgnstring)-1], 100)
+	var play []Move
 	for _, val := range movslic {
 		move := strings.Split(val, " ")
 		if len(move) == 4 {
-			log.Println(move[1], "---", move[2])
+			play = append(play, Move{White: move[1], Black: move[2]})
 		} else {
 			log.Println(len(move))
 		}
 
 	}
-	return &PGN{Meta: getmeta}
+	return &PGN{Meta: getmeta, Moves: play}
+}
+
+func (p *PGN) String() string {
+	var movstring string
+	for _, mov := range p.Moves {
+		movstring = fmt.Sprintf("%s%s%s", movstring, mov.White, mov.Black)
+	}
+	return movstring
+}
+
+func GetPiece(move string) string {
+	return ""
 }
